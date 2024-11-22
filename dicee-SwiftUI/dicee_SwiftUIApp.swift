ef55fe2b2a3d700daa61faf5638f329d9db6980f
Melissa Elliston-Boyes <melissa.elliston-boyes@trademe.co.nz>
2024-11-22 16:46:43 +1300

    Initial Commit

---
+++ dicee-SwiftUI/dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  dicee_SwiftUIApp.swift
+//  dicee-SwiftUI
+//
+//  Created by Melissa Elliston-Boyes on 22/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
